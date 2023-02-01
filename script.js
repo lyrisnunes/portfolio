19a6296677ba84493df5689ca2d8f925267dc712
lyrisnunes~ <lyrisnunesdasilva@gmail.com>
2023-02-01 20:49:14 -0300

    projeto

--- script.js
+++ script.js
@@ -7,3 +7,27 @@
         }
     }
  
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      slidesPerGroup: 2,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    
+      breakpoints:{
+        0: {
+          slidesPerView:1,
+        },
+        520: {
+          slidesPerView: 2,
+        }
+      }
+    });
